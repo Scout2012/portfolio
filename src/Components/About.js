449868b683032fe82331d0ed9c44e22cff36bc51
Scout2012 <jcbpowell12@gmail.com>
2020-07-11 20:42:27 -0700

    revert to view instead of download

--- src/Components/About.js
+++ src/Components/About.js
@@ -51,7 +51,7 @@ class About extends Component {
             <div className="row">
                <div className="columns download">
                   <p>
-                     <button className="button" href={resumeDownload}><i className="fa fa-download"></i>View Resume</button>
+                     <a className="button" href={resumeDownload}><i className="fa fa-download"></i>View Resume</a>
                   </p>
                </div>
             </div>
